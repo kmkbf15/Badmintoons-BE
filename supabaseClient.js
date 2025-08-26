8b6560ca60e20826db0f488e3dd282c25df5dac7
kmkbf15 <maxwellgunawan@yahoo.com>
2025-08-26 14:38:32 +0700

    Restructure for Vercel deployment

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,9 +1,9 @@
 // supabaseClient.js
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_ANON_KEY;
-
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
+);
 
 export default supabase;
