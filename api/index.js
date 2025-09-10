df66e9319dbcdcedac2ecfd7ef74353b8deeb952
kmkbf15 <maxwellgunawan@yahoo.com>
2025-09-10 09:47:57 +0700

    add patch available

--- api/index.js
+++ api/index.js
@@ -17,7 +17,10 @@ app.use(express.urlencoded({ extended: true }));
 // CORS if needed
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,PUT,POST,DELETE,OPTIONS,PATCH"
+  );
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
